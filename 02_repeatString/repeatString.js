a3eec303adf699e54dc63ce96647077a07c6766a
Austin Hogan <austinhogandev@gmail.com>
2023-08-10 11:43:25 -0400

    02-tdd-repeatingstring

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (text, reps) {
+    if (reps < 0) return "ERROR";
+    let res = "";
+    for (let i = 0; i < reps; i++) {
+        res += text;
+    }
+    return res;
 };
 
 // Do not edit below this line
