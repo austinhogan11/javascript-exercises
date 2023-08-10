6871cfc6b651e2f6c3a6ebabfecc14070220ec95
Austin Hogan <austinhogandev@gmail.com>
2023-08-10 11:59:30 -0400

    03 Reverse String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,22 @@
-const reverseString = function() {
+const reverseString = function (text) {
+    // 1. Built in methods
+    // split = text.split("");
+    // reversed = split.reverse();
+    // joined = reversed.join("");
+    // return joined;
+    
+    // Same as 1. but better
+    return text.split("").reverse().join("");
+
+    // for loop
+    // let res = "";
+    // for (let i = text.length - 1; i >= 0; i--) {
+    //     res += text[i];
+    // }
+
+    // return res;
+
+
 
 };
 
