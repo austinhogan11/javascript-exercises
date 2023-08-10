3c6292cb11ac69ff7a1e43608cd57f9d97226cdd
Austin Hogan <austinhogandev@gmail.com>
2023-08-10 12:24:24 -0400

    04 Remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array) {
+    //Use arguments for a variable amount of arguments
+    // Loop through number of arguments, index 0 = array, next indexes are items to remove
+    for (let x = 1; x < arguments.length; x++) {
+        // Loop through array checking if arguments match array eleemnts
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === arguments[x]) {
+                if (i === array.length - 1) {
+                    array.pop();
+                } else {
+                    array.splice(i, 1);
+                }
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
